83f54f0324d6ff6af0ad6c91eb724fd9ccddeae8
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-01-30 17:09:46 -0500

    seeting upp API

---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+//getting the info from Random user API 
+const BASEURL = "https://randomuser.me/api/?results=5&inc=name,gender,nat&noinfo";
+
+
+export default {
+  search: ()=> {
+    return axios.get(BASEURL);
+  }
+};
