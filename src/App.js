fc071b8e0b6e103c86b981ac6d880244bfd0d293
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-01-30 23:27:10 -0500

    creating components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Hero from "./components/Hero"
+import Main from "./pages/Main";
+import Footer from "./components/Footer"
 
 function App() {
   return (
- <Router>
-   <Hero />
- </Router>
+      <div>
+         <Hero>
+          {/* <Hero backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg7njMApBZ1PyHUSg5cKmojRKQti_JvTRznw&usqp=CAU"> */}
+        <h1> Welcome to the Employee Directory </h1>
+        </Hero>
+      <Footer />
+      </div>
+  
+    // <Hero />
+//  <Router>
+//    <div>
+//     <Route exact path="/" component={Main} />
+//     <Route exact path="/main" component={Main} />
+//    </div>
+   
+//  </Router>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+                <a href="https://www.linkedin.com/in/rodolfo-diaz-2371b819/" target="_blank"><i className="fab fa-linkedin fa-2x"></i></a>
+                <a href="https://github.com/Rodolfod1" target="_blank"><i className="fab fa-github fa-2x"></i> </a> 
+                <p className="copyright">© 2021 Rodolfo Diaz</p>
+        </footer>
+        
+    )
+}
+
+export default Footer
